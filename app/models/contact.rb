a483cb1f1e647d207418f0881f7b44f661387860
mathiasbarreto <mathias.barreto@cashway.io>
2021-10-21 17:55:58 -0300

    curso em andamento

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,18 @@
 class Contact < ApplicationRecord
+  belongs_to :kind, optional: true
+  def author
+      'Jackson'
+  end
+
+  def kind_description
+    self.kind.description
+  end
+
+  def as_json(options = {})
+    super(
+          root: true,
+          methods: [:kind_description, :author],
+          include: { kind: { only: :description }}
+          )
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,22 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts ' Cadastrando os tipos de contato...'
+kinds = %w(Amigo Comercial Conhecido)
+kinds.each do |kind|
+  Kind.create!(description: kind)
+end
+puts 'Tipos de contatos cadastrados com sucesso!'
+
+##################################
+
 p "Cadastrando os contatos"
 100.times do |i|
     Contact.create!(
       name: Faker::Name.name,
       email:Faker::Internet.email,
-      birthdate: Faker::Date.between(from: 65.years.ago, to: 18.years.ago)
+      birthdate: Faker::Date.between(from: 65.years.ago, to: 18.years.ago),
+      kind: Kind.all.sample
     )
   end
 p "Contatos cadastrados com sucesso!"
